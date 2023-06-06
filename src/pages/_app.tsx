446a5d801888668398a9bf68a08cc524609547e5
LeafField <109466326+LeafField@users.noreply.github.com>
2023-06-06 18:02:53 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SNS Udemy</title>
+        <meta name="description" content="UdemyのSNSアプリ作成講座です" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
