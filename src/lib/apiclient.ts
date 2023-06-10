6dffc6b3fd40561dd85c7777ecacc8b6e53c97c2
LeafField <109466326+LeafField@users.noreply.github.com>
2023-06-10 22:32:03 +0900

    軽微な修正

--- src/lib/apiclient.ts
+++ src/lib/apiclient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASEURL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
